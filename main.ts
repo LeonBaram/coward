eb4318e9e0e9e79feb9f1f2302009c2a4e8e1c61
LeonBaram <leonbaram@protonmail.com>
2023-02-19 17:34:27 -0500

    Rewrite main.ts
    main.ts now follows basic example from discord.js documentation

--- main.ts
+++ main.ts
@@ -1,1 +1,39 @@
-console.log("hi");
+import { ChatInputCommandInteraction, Client, Events, GatewayIntentBits, SlashCommandBuilder } from "discord.js";
+import { token } from "./config.json";
+import * as commands from "./commands";
+
+const { Guilds, GuildVoiceStates, GuildMessages } = GatewayIntentBits;
+
+const client = new Client({ intents: [Guilds, GuildVoiceStates, GuildMessages] });
+
+client.once(Events.ClientReady, (client) =>
+	console.log(`client is ready; logged in as ${client.user.tag}`));
+
+client.login(token);
+
+client.on(Events.InteractionCreate, async (interaction) => {
+	if (!interaction.isChatInputCommand()) {
+		return;
+	}
+
+	const { commandName } = interaction;
+
+	if (!(commandName in commands)) {
+		console.error(`cannot execute command ${commandName}; no such command`);
+		return;
+	}
+
+	try {
+		type Command = {
+			data: SlashCommandBuilder,
+			execute: (interaction: ChatInputCommandInteraction) => Promise<undefined>,
+		};
+		await (commands[commandName] as Command).execute(interaction);
+	} catch (error) {
+		console.error(error);
+		await interaction.reply({
+			content: `there was an error while executing command "${commandName}"`,
+			ephemeral: true,
+		});
+	}
+});
