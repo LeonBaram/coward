634d54e8583e42a8dda23a459fc4011849434b38
LeonBaram <leonbaram@protonmail.com>
2023-02-28 16:07:40 -0500

    Add more checks to "play" command
    checks to make sure that:
    - user is in a server
    - user is in a voice chat
    - bot is not currently being used in a different voice chat

--- commands/play.ts
+++ commands/play.ts
@@ -1,9 +1,58 @@
-import { ChatInputCommandInteraction, SlashCommandBuilder } from "discord.js";
+import { Player } from "discord-player";
+import { SlashCommandBuilder } from "discord.js";
+import type {
+  ChatInputCommandInteraction,
+  GuildMember,
+  InteractionResponse
+} from "discord.js";
 
 export const data = new SlashCommandBuilder()
   .setName("play")
   .setDescription("add track to start of queue and play it, or unpause current track");
 
-export const execute = async (interaction: ChatInputCommandInteraction) => {
-  await interaction.reply(`playing track "bepis" in "joe mama"`);
+export const execute = async (
+  interaction: ChatInputCommandInteraction,
+  player: Player
+): Promise<InteractionResponse<boolean>> => {
+  if (!interaction.guild) {
+    return interaction.reply({
+      content: "you must be in a server to use this command",
+      ephemeral: true,
+    });
+  }
+
+  const user = interaction.member as GuildMember;
+
+  if (!user.voice.channel) {
+    return interaction.reply({
+      content: "you must be in a voice chat to use this command",
+      ephemeral: true,
+    });
+  }
+
+  const botChannel = interaction.guild?.members.me?.voice.channel;
+
+  if (botChannel && (botChannel.id !== user.voice.channel!.id)) {
+    return interaction.reply({
+      content: `already playing in "${botChannel.name}"`,
+      ephemeral: true,
+    });
+  }
+
+  const query = interaction.options.getString("query");
+
+  if (query === null) {
+    // unpause current track
+  } else {
+    const queue = player.createQueue(interaction.guild, {
+      metadata: {
+        channel: interaction.channel,
+      },
+    });
+  }
+
+  return interaction.reply({
+    content: `playing track "bepis" in "joe mama"`,
+    ephemeral: true,
+  });
 };
